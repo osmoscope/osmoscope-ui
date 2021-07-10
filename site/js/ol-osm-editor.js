ffb18644dfa3c7225f6ea7a7a51b98b76a46af76
Jochen Topf <jochen@topf.org>
2021-07-10 14:14:45 +0200

    Use more modern Javascript for the osm editor control

--- site/js/ol-osm-editor.js
+++ site/js/ol-osm-editor.js
@@ -1,47 +1,56 @@
 
-var EditInOSMControl = function(opt_options) {
+class EditInOSMControl extends ol.control.Control {
 
-    var round_coord = function(value) {
-        return Math.round(value * 10000) / 10000;
-    };
+    constructor(opt_options) {
+        var options = opt_options || {};
 
-    var josm_url = function(extent, selection) {
-        var bl = ol.proj.transform(extent.slice(0, 2), 'EPSG:3857', 'EPSG:4326');
-        var tr = ol.proj.transform(extent.slice(2, 4), 'EPSG:3857', 'EPSG:4326');
-        var uri = 'http://127.0.0.1:8111/load_and_zoom'
-            + '?left='   + round_coord(bl[0])
-            + '&right='  + round_coord(tr[0])
-            + '&top='    + round_coord(tr[1])
-            + '&bottom=' + round_coord(bl[1])
-//        + '&changeset_comment=xxx';
-
-        if (selection !== undefined) {
-            uri += '&select=' + selection;
+        if (options.editor === undefined) {
+            console.log('Missing editor option');
+            return;
+        } else if (options.editor === 'josm') {
+            options.title = 'Open current view in JOSM';
+            options.icon = 'J';
+            options.className = 'edit-in-osm-control-josm';
+        } else if (options.editor === 'id') {
+            options.title = 'Open current view in iD';
+            options.icon = 'iD';
+            options.className = 'edit-in-osm-control-id';
+        } else {
+            console.log('Unknown editor: ' + options.editor);
+            return;
         }
 
-        return uri;
-    };
+        const button = document.createElement('button');
+        button.setAttribute('title', options.title);
+        button.innerHTML = options.icon;
 
-    this.open_in_josm = function(extent, selection) {
-        d3.text(josm_url(extent, selection)).then(function(t){
-            if (t.indexOf('OK') === -1) {
-                show_message("Can not reach JOSM. Is it running?");
-            } else {
-                show_message("Loading data into JOSM...");
-            }
+        const element = document.createElement('div');
+        element.className = options.className + ' ol-unselectable ol-control';
+        element.appendChild(button);
+
+        super({
+            element: element,
+            target: options.target
         });
-    };
 
-    var open_in_josm_with_view = function() {
-        this_.open_in_josm(this_.getMap().getView().calculateExtent());
-    };
+        const openEditor =
+            (options.editor === 'josm' ? this.open_in_josm_with_view.bind(this)
+                                       : this.open_in_id_with_view.bind(this));
 
-    var id_url = function(center, selection) {
-        var xy = ol.proj.transform(center, 'EPSG:3857', 'EPSG:4326');
-        var zoom = this_.getMap().getView().getZoom();
+        button.addEventListener('click', openEditor, false);
+        button.addEventListener('touchstart', openEditor, false);
+    }
+
+    round_coord(value) {
+        return Math.round(value * 10000) / 10000;
+    }
+
+    id_url(center, selection) {
+        const xy = ol.proj.transform(center, 'EPSG:3857', 'EPSG:4326');
+        const zoom = this.getMap().getView().getZoom();
         var uri = 'https://www.openstreetmap.org/edit?editor=id'
-            + '&lon=' + round_coord(xy[0])
-            + '&lat=' + round_coord(xy[1])
+            + '&lon=' + this.round_coord(xy[0])
+            + '&lat=' + this.round_coord(xy[1])
             + '&zoom=' + zoom;
 //        + '&comment=xxx';
 
@@ -51,54 +60,46 @@ var EditInOSMControl = function(opt_options) {
 
         console.log(uri);
         return uri;
-    };
-
-    this.open_in_id = function(center, selection) {
-        window.open(id_url(center, selection), 'OSMIdWindow');
-    };
-
-    var open_in_id_with_view = function() {
-        this_.open_in_id(map.getView().getCenter());
-    };
-
-    var options = opt_options || {};
-
-    if (options.editor === undefined) {
-    } else if (options.editor === 'josm') {
-        options.openEditor = open_in_josm_with_view;
-        options.title = 'Open current view in JOSM';
-        options.icon = 'J';
-        options.className = 'edit-in-osm-control-josm';
-    } else if (options.editor === 'id') {
-        options.openEditor = open_in_id_with_view;
-        options.title = 'Open current view in iD';
-        options.icon = 'iD';
-        options.className = 'edit-in-osm-control-id';
-    } else {
-        console.log('Unknown editor: ' + options.editor);
-        return;
     }
 
-    var button = document.createElement('button');
-    button.setAttribute('title', options.title);
-    button.innerHTML = options.icon;
+    open_in_id(center, selection) {
+        window.open(this.id_url(center, selection), 'OSMIdWindow');
+    }
 
-    var this_ = this;
-    var openEditor = options.openEditor;
-    var zoomThreshold = options.zoomThreshold || 15;
+    open_in_id_with_view() {
+        this.open_in_id(this.getMap().getView().getCenter());
+    }
 
-    button.addEventListener('click', openEditor, false);
-    button.addEventListener('touchstart', openEditor, false);
+    josm_url(extent, selection) {
+        const bl = ol.proj.transform(extent.slice(0, 2), 'EPSG:3857', 'EPSG:4326');
+        const tr = ol.proj.transform(extent.slice(2, 4), 'EPSG:3857', 'EPSG:4326');
+        var uri = 'http://127.0.0.1:8111/load_and_zoom'
+            + '?left='   + this.round_coord(bl[0])
+            + '&right='  + this.round_coord(tr[0])
+            + '&top='    + this.round_coord(tr[1])
+            + '&bottom=' + this.round_coord(bl[1])
+//        + '&changeset_comment=xxx';
 
-    var element = document.createElement('div');
-    element.className = options.className + ' ol-unselectable ol-control';
-    element.appendChild(button);
+        if (selection !== undefined) {
+            uri += '&select=' + selection;
+        }
 
-    ol.control.Control.call(this, {
-        element: element,
-        target: options.target
-    });
-};
+        return uri;
+    }
 
-ol.inherits(EditInOSMControl, ol.control.Control);
+    open_in_josm(extent, selection) {
+        d3.text(this.josm_url(extent, selection)).then(function(t){
+            if (t.indexOf('OK') === -1) {
+                show_message("Can not reach JOSM. Is it running?");
+            } else {
+                show_message("Loading data into JOSM...");
+            }
+        });
+    }
+
+    open_in_josm_with_view() {
+        this.open_in_josm(this.getMap().getView().calculateExtent());
+    }
+
+}
 
